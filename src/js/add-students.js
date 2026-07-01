5e12f115687765eaa89e7869975ddcf64118100d
Lev-Plo <lev.plotnyk@gmail.com>
2026-07-01 16:45:59 +0200

    now supposed to work

--- src/js/add-students.js
+++ src/js/add-students.js
@@ -1,20 +1,19 @@
 export async function addStudent(data) {
-    const options = {
-        method:"POST",
-        body:JSON.stringify(data),
-        headers: {
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
       "Content-Type": "application/json; charset=UTF-8",
+    },
+  };
+  try {
+    const res = await fetch("http://localhost:3000/students", options);
+    if (!res.ok) {
+      throw new Error(`HTTP Error: ${res.status}`);
     }
-    }
-    try {
-      const res = await fetch("http://localhost:3000/students",options);
-      if (!res.ok) {
-        throw new Error(error.messages);
-        
-      }
-      const info = await res.json();
-      return info
-    } catch (error) {
-      throw error
-    }
+    const info = await res.json();
+    return info;
+  } catch (error) {
+    throw error;
+  }
 }
